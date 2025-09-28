cb1c5e3d38af237fa0ad1cf8f023287188940cf0
Dmitry Sergeev <justbringit16@yandex.ru>
2025-09-28 23:56:21 +0300

    Добавлен dispatcher во все usecase

--- app/src/main/java/com/dmitrysergeev/translateapp/domain/addfavouritewordtranslationusecase/AddFavouriteWordTranslationUseCaseImpl.kt
+++ app/src/main/java/com/dmitrysergeev/translateapp/domain/addfavouritewordtranslationusecase/AddFavouriteWordTranslationUseCaseImpl.kt
@@ -2,13 +2,17 @@ package com.dmitrysergeev.translateapp.domain.addfavouritewordtranslationusecase
 
 import com.dmitrysergeev.translateapp.data.translation.TranslationRepository
 import com.dmitrysergeev.translateapp.data.translation.entities.WordTranslation
+import com.dmitrysergeev.translateapp.di.IoDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AddFavouriteWordTranslationUseCaseImpl @Inject constructor(
-    private val translationRepository: TranslationRepository
+    private val translationRepository: TranslationRepository,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
 ): AddFavouriteWordTranslationUseCase {
 
-    override suspend operator fun invoke(worldTranslation:WordTranslation){
+    override suspend operator fun invoke(worldTranslation:WordTranslation) = withContext(dispatcher){
         translationRepository.addFavourite(worldTranslation)
     }
 
--- app/src/main/java/com/dmitrysergeev/translateapp/domain/gettranslationsforqueryusecase/GetTranslationsForQueryUseCaseImpl.kt
+++ app/src/main/java/com/dmitrysergeev/translateapp/domain/gettranslationsforqueryusecase/GetTranslationsForQueryUseCaseImpl.kt
@@ -2,15 +2,21 @@ package com.dmitrysergeev.translateapp.domain.gettranslationsforqueryusecase
 
 import com.dmitrysergeev.translateapp.data.translation.TranslationRepository
 import com.dmitrysergeev.translateapp.data.translation.entities.WordTranslation
+import com.dmitrysergeev.translateapp.di.IoDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class GetTranslationsForQueryUseCaseImpl @Inject constructor(
-    private val translationRepository: TranslationRepository
+    private val translationRepository: TranslationRepository,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
 ): GetTranslationsForQueryUseCase {
 
     override operator fun invoke(query: String): Flow<List<WordTranslation>> {
-        return translationRepository.getTranslations(query)
+        return translationRepository
+            .getTranslations(query)
+            .flowOn(dispatcher)
     }
 
 }
