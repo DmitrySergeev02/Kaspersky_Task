fce72fee462a93674374f79f7628dcbd9f1af180
Dmitry Sergeev <justbringit16@yandex.ru>
2025-09-29 23:06:43 +0300

    красный цвет для кнопки добавления в избранное

--- app/src/main/java/com/dmitrysergeev/translateapp/ui/favouritesscreen/recyclerview/FavouritesViewHolder.kt
+++ app/src/main/java/com/dmitrysergeev/translateapp/ui/favouritesscreen/recyclerview/FavouritesViewHolder.kt
@@ -1,5 +1,6 @@
 package com.dmitrysergeev.translateapp.ui.favouritesscreen.recyclerview
 
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.dmitrysergeev.translateapp.R
 import com.dmitrysergeev.translateapp.data.translation.db.favourites.FavouriteDbEntity
@@ -15,6 +16,7 @@ class FavouritesViewHolder(
         binding.baseWord.text = resources.getString(R.string.translate_item_base_word, item.originalWord)
         binding.translationWord.text  = resources.getString(R.string.translate_item_translation, item.translation)
         binding.actionIcon.setImageResource(R.drawable.broken_heart_icon)
+        binding.actionIcon.drawable.setTint(ContextCompat.getColor(binding.root.context, R.color.red))
         binding.actionIcon.setOnClickListener {
             onClick(item)
         }
